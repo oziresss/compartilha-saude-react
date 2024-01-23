a7429767beb4adde95c80bd265a3f0f4670127d8
Ozires Alves <100055655+oziresss@users.noreply.github.com>
2024-01-23 14:13:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './main.css';
-import App from './App';
-import ProdutoProvider from './contexts/ProdutoContexto';
+import { React } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../img/Logo.png';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ProdutoProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ProdutoProvider>
-);
+
+const RecuperaSenha = () => {
+    const handleResetRequest = async (event) => {
+        event.preventDefault();
+    
+        const email = document.getElementById('email').value;
+    
+        try {
+          const response = await fetch('https://compartilhasaude-backend-di7vbasruq-rj.a.run.app/api/User/SendEmailValidate', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+          });
+    
+          if (response.ok) {
+            aleert('Solicitação de recuperação de senha enviada com sucesso.');
+          } else {
+            const errorData = await response.json();
+            alert(errorData[0].message);
+          }
+        } catch (error) {
+          alert('Erro durante a solicitação de recuperação de senha:', error);
+        }
+    };
+
+    return ( 
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-20 w-auto"
+            src={Logo}
+            alt="Your Company"
+          />
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                  Recuperação de Senha
+              </h2>
+            </div>
+    
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                      Endereço de email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div> 
+                <div>
+                  <button
+                    onClick={handleResetRequest}
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Enviar
+                  </button>
+                </div>
+                <p className="mt-5 text-center text-sm text-gray-500">
+                  Já tem uma conta?{' '}
+                    <Link to="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                      Faça login
+                    </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+  
+     );
+}
+ 
+export default RecuperaSenha;
